7f0ee9c9b9cac0a92cf7542081ca3c57bc63a650
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2023-11-23 10:10:31 +0100

    Add files via upload

---
+++ iswm1.py
@@ -0,0 +1,42 @@
+import re
+import pytube as pt
+import json
+
+
+data = {}
+lastchoice = ""
+
+def get_desc(youtube):
+    stream = youtube.streams.first()
+    desc = youtube.description
+    return desc
+
+def IndexVideo(url):
+    global data
+    global lastchoice
+    youtube = pt.YouTube(url)
+    text = get_desc(youtube)
+    
+    if youtube.title == "In Space with Markiplier: Part 2" or text == None:
+        return
+    
+    print("Links: " + text)
+
+    links = re.findall(r"https?://\S+", text)
+    for link in links:
+        print(youtube.title)
+        if link in data:
+            return
+        data[link] = {
+            "link": link,
+            "parent": lastchoice,
+            "name": youtube.title
+        }
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+        lastchoice = link
+        IndexVideo(link)
+        
+
+IndexVideo("https://www.youtube.com/watch?v=j64oZLF443g")
+print(data)
